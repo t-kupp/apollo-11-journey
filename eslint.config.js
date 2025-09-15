70ef7ac95652a88aba2e48525ab528b08d6877dd
Jan <kupper.thorge@gmail.com>
2025-09-15 14:37:39 +0200

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,51 @@
+import js from "@eslint/js";
+import eslintPluginJsxA11y from "eslint-plugin-jsx-a11y";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import { globalIgnores } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config([
+  globalIgnores(["dist"]),
+  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs["recommended-latest"],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: { prettier: eslintPluginPrettier, "jsx-a11y": eslintPluginJsxA11y },
+    rules: {
+      "prettier/prettier": [
+        "error",
+        {
+          trailingComma: "es5",
+          semi: true,
+          tabWidth: 2,
+          singleQuote: false,
+          printWidth: 100,
+          endOfLine: "auto",
+          arrowParens: "always",
+          plugins: ["prettier-plugin-tailwindcss"],
+        },
+        {
+          usePrettierrc: false,
+        },
+      ],
+      "react/react-in-jsx-scope": "off",
+      "jsx-a11y/alt-text": "warn",
+      "jsx-a11y/aria-props": "warn",
+      "jsx-a11y/aria-proptypes": "warn",
+      "jsx-a11y/aria-unsupported-elements": "warn",
+      "jsx-a11y/role-has-required-aria-props": "warn",
+      "jsx-a11y/role-supports-aria-props": "warn",
+    },
+  },
+]);
